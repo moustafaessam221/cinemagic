18f7db6d89a542b744568ff0d9d345d7b3136c7c
moustafaessam221 <moustafaessam991@gmail.com>
2024-11-20 18:07:09 +0200

    Add Movie Details: Ahmed Aziz

--- src/pages/movieDetails/Components/MovieInfo.jsx
+++ src/pages/movieDetails/Components/MovieInfo.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "../MovieDetails.css";
-import { useParams } from "react-router-dom";
 
 function MovieInfo({ movie }) {
   if (!movie) {
@@ -9,16 +8,22 @@ function MovieInfo({ movie }) {
 
   return (
     <div className="movie-info-column">
-      <h2>{movie.title}</h2>
-      <img src={movie.Poster} alt={movie.Title} />
-      <p>Release Date: {movie.Released}</p>
-      <p>Language: {movie.Language}</p>
-      <p>Genre: {movie.Genre}</p>
-      <p>Director: {movie.Director}</p>
-      <p>Writers: {movie.Writer}</p>
-      <p>Rated: {movie.Rated}</p>
-      <p>Awards: {movie.Awards}</p>
-      {/* ... other movie details */}
+      <div className="main">
+        <img src={movie.Poster} alt={movie.Title} />
+        <div className="poster-overlay">
+          <h2>{movie.Title}</h2>
+          <button className="play-trailer-btn">Play Trailer</button>
+        </div>
+      </div>
+      <div className="info">
+        <p>Release Date: {movie.Released}</p>
+        <p>Language: {movie.Language}</p>
+        <p>Genre: {movie.Genre}</p>
+        <p>Director: {movie.Director}</p>
+        <p>Writers: {movie.Writer}</p>
+        <p>Rated: {movie.Rated}</p>
+        <p>Awards: {movie.Awards}</p>
+      </div>
     </div>
   );
 }
